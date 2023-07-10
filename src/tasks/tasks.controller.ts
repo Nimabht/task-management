8fc6223190f5f46449ce50f20754faa12e0af08e
nimabht <nimabeheshtaein99@gmail.com>
2023-07-11 00:51:38 +0330

    [feat] task controller created.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Delete } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { CreateTaskDto } from './dto/createTask.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get('/:taskId')
+  getTaskById(@Param('taskId') taskId: string): Task {
+    return this.tasksService.getTaskById(taskId);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:taskId')
+  deleteTaskById(@Param('taskId') taskId: string): void {
+    this.tasksService.deleteTask(taskId);
+  }
 }
