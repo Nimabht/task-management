c6c5a1dd7872472fe9bdc72caadf599376f95b60
nimabht <nimabeheshtaein99@gmail.com>
2023-07-11 16:33:21 +0330

    [refactor] dtos refactored.

--- src/tasks/dto/updateTaskStatus.dto.ts
+++ src/tasks/dto/updateTaskStatus.dto.ts
@@ -1,4 +1,4 @@
-import { TaskStatus } from '../task.model';
+import { TaskStatus } from '../taskStatus.enum';
 import { IsEnum } from 'class-validator';
 export class UpdateTaskStatusDto {
   @IsEnum(TaskStatus)
